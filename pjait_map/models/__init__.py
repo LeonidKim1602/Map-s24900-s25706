2f377fba690382aaa0cca723d1399c57afa2b2b8
Le0-dot <lkolyadich@gmail.com>
2023-05-27 14:10:43 +0200

    Remove redundant models. Add Location and File models

--- pjait_map/models/__init__.py
+++ pjait_map/models/__init__.py
@@ -1,7 +1,5 @@
 from .registry import metadata
 from .student import Student
-from .building import Building
-from .floor import Floor
-from .room import Room
+from .room import Room, Location, File
 from .subject import Subject
 from .schedule import Schedule
--- pjait_map/models/room.py
+++ pjait_map/models/room.py
@@ -11,9 +11,35 @@ class Room:
     id: Mapped[int] = mapped_column('RoomId', primary_key=True)
     name: Mapped[str] = mapped_column('RoomName')
 
-    floor_id: Mapped[int] = mapped_column(ForeignKey('Floor.FloorId'))
-    floor: Mapped['Floor'] = relationship(back_populates='Floor')
+    location_id: Mapped[int] = mapped_column(ForeignKey('Location.LocationId'))
+    location: Mapped['Location'] = relationship(back_populates='Location')
 
     schedules: Mapped[list['Schedule']] = relationship(
         back_populates='Schedule', default_factory=lambda: []
+    )
+
+@mapper_registry.mapped_as_dataclass
+class Location:
+    __tablename__ = 'Location'
+
+    id: Mapped[int] = mapped_column('LocationId', primary_key=True)
+    x: Mapped[int] = mapped_column('X')
+    y: Mapped[int] = mapped_column('Y')
+
+    file_id: Mapped[int] = mapped_column(ForeignKey('File.FileId'))
+    file: Mapped['File'] = relationship(back_populates='File')
+
+    rooms: Mapped[list['Room']] = relationship(
+        back_populates='Room', default_factory=lambda: []
+    )
+
+@mapper_registry.mapped_as_dataclass
+class File:
+    __tablename__ = 'File'
+
+    id: Mapped[int] = mapped_column('FileId', primaty_key=True)
+    name: Mapped[str] = mapped_column('Filename')
+
+    locations: Mapped[list['Location']] = relationship(
+        back_populates='Location', default_factory=lambda: []
     )
