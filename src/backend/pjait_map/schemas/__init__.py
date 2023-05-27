743e47c83d4d26d8f8940588412e2fcf6ce6a5de
Le0-dot <lkolyadich@gmail.com>
2023-05-27 16:19:09 +0200

    Add schemas to the project

---
+++ src/backend/pjait_map/schemas/__init__.py
@@ -0,0 +1,2 @@
+from .login import LoginData
+from .class_schemas import Position, ClassLocation, ClassInfo, ClassUpdate
---
+++ src/backend/pjait_map/schemas/class_schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class Position(BaseModel):
+    x: int
+    y: int
+
+class ClassLocation(BaseModel):
+    room: str
+    file: str
+    pos: Position # x and y coordinates on the image
+
+class ClassInfo(BaseModel):
+    subject_id: int
+    room_id: int
+    weekday: int
+    start: str # start and end in "hh:mm" format,
+    end: str   # so for now they are str
+
+class ClassUpdate(BaseModel):
+    schedule_id: int
+    data: ClassInfo
