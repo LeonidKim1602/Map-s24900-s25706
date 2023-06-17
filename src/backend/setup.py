4cc6d8b4cbc000ad55ac67193603d4f44a5eff8b
Le0-dot <lkolyadich@gmail.com>
2023-06-17 17:27:06 +0200

    Fix setup.py for backend.

--- src/backend/setup.py
+++ src/backend/setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md") as buffer:
-    long_description = buffer.read()
-
 setuptools.setup(
     name="pjait_map_backend",
     version="0.0.1",
     author="Lev Koliadich",
     author_email="s25706@pjwstk.edu.pl",
     description="Interactive map of PJAIT backend part",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/LeonidKim1602/Map-s24900-s25706",
     packages=setuptools.find_packages(),
     classifiers=[
