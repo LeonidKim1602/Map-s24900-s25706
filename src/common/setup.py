28f6b63d1d0865019e6d040291683600ff8fe0cd
Le0-dot <lkolyadich@gmail.com>
2023-06-17 15:51:36 +0200

    Fix setup.py

--- src/common/setup.py
+++ src/common/setup.py
@@ -1,22 +1,11 @@
 import setuptools
 
-<<<<<<< HEAD
-=======
-with open("README.md") as buffer:
-    long_description = buffer.read()
-
->>>>>>> 3cd6c0a33f07ec9fcf38baaf7ee0092ee5a90fd3
 setuptools.setup(
     name="pjait_map_common",
     version="0.0.1",
     author="Lev Koliadich",
     author_email="s25706@pjwstk.edu.pl",
     description="Interactive map of PJAIT",
-<<<<<<< HEAD
-=======
-    long_description=long_description,
-    long_description_content_type="text/markdown",
->>>>>>> 3cd6c0a33f07ec9fcf38baaf7ee0092ee5a90fd3
     url="https://github.com/LeonidKim1602/Map-s24900-s25706",
     packages=setuptools.find_packages(),
     classifiers=[
