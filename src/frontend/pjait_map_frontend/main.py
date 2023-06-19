33bde7c032bf10a2705c67b7f03830c2f519eeae
Leonid Kim <s24900@pjwstk.edu.pl>
2023-06-19 18:57:52 +0200

    Add index and timetable endpoints

--- src/frontend/pjait_map_frontend/main.py
+++ src/frontend/pjait_map_frontend/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 import requests
+import datetime
 from fastapi import FastAPI, Form, Request, Response, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.responses import HTMLResponse
@@ -10,7 +11,7 @@ from pydantic import parse_raw_as
 
 from pjait_map_common.schemas import *
 from uuid import UUID
-from session import create_session, delete_session, SessionData, cookie
+from session import create_session, delete_session, SessionData, cookie, verifier
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -26,7 +27,7 @@ def login(request: Request) -> HTMLResponse:
 async def login(username: Annotated[str, Form()], password: Annotated[str, Form()], request: Request) -> Response:
 	request_user = User(number=int(username[1:]), password=password)
 	response = requests.post("http://localhost:8001/student", json=request_user.dict())
-	
+
 	if response.status_code == 200:
 		print("Ok")
 		data = SessionData(student_id=request_user.number)
@@ -45,11 +46,36 @@ async def login(username: Annotated[str, Form()], password: Annotated[str, Form(
 	else:
 		print("MEGAERROR")
 		return None
+
 		
-@app.post("/")
-@app.get("/")
-async def index(session_id: UUID = Depends(cookie)):
-	response = Response()
-	print(session_id)
+@app.post("/", dependencies=[Depends(cookie)])
+@app.get("/", dependencies=[Depends(cookie)])
+async def index(request: Request, session_data: SessionData = Depends(verifier)):
+	response = requests.get(f"http://localhost:8001/schedule/student/{session_data.student_id}")
+	response_schedules = parse_raw_as(list[Schedule], response.text)
+	weekday = datetime.datetime.today().weekday()
+	today = [s for s in response_schedules if s.weekday == weekday]
+	locations = []
+	
+	for schedule in today:
+		resp = requests.get(f"http://localhost:8001/room/{schedule.room_id}")
+		locations.append(parse_raw_as(ClassLocation, resp.text))
+	
+	subjects_today = zip(today, locations)
+	
+	return templates.TemplateResponse("main.html", {"request": request, "subjects_today": subjects_today})
 	#await delete_session(response, session_id)
-	return response
+	
+@app.get("/timetable", dependencies=[Depends(cookie)])
+def timetable(request: Request, session_data: SessionData = Depends(verifier)):
+	response = requests.get(f"http://localhost:8001/schedule/student/{session_data.student_id}")
+	response_schedules = parse_raw_as(list[Schedule], response.text)
+	
+	days = []
+	for day in range(5):
+		days.append([s for s in response_schedules if s.weekday == day])
+		
+	days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+	
+	return templates.TemplateResponse("timetable.html", {"request": request, "days": zip(days, days_of_the_week)})
+	
