0e060c5658223784142c5dc91645dfe93231c6b3
Le0-dot <lkolyadich@gmail.com>
2023-06-19 21:06:23 +0200

    Move img folder into static/img

--- src/backend/pjait_map_backend/main.py
+++ src/backend/pjait_map_backend/main.py
@@ -9,7 +9,7 @@ from routers import schedule_router, room_router, subject_router, student_router
 
 app = FastAPI(dependencies=[Depends(DatabaseManager.get_db)])
 
-app.mount("/img", StaticFiles(directory="img"), name="img")
+app.mount("/img", StaticFiles(directory="static/img"), name="img")
 
 app.include_router(schedule_router)
 app.include_router(room_router)
