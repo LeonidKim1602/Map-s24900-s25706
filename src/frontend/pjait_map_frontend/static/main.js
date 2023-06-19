706603b9f959523a1ec3873aef727c4f23b60fc5
Leonid Kim <s24900@pjwstk.edu.pl>
2023-06-19 18:56:52 +0200

    Fix javascript

--- src/frontend/pjait_map_frontend/static/main.js
+++ src/frontend/pjait_map_frontend/static/main.js
@@ -1,30 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Retrieve the subject list from the backend API
-  fetch("/api/subjects")
-    .then(response => response.json())
-    .then(subjects => {
-      const subjectList = document.getElementById("subject-list");
-      subjects.forEach(subject => {
-        const listItem = document.createElement("li");
-        listItem.textContent = subject.name;
-        listItem.addEventListener("click", () => {
-          // Fetch and display the image corresponding to the selected subject
-          fetch(`/api/subject/${subject.id}/file`)
-            .then(response => response.blob())
-            .then(blob => {
-              const imageURL = URL.createObjectURL(blob);
-              displayImage(imageURL);
-            });
-        });
-        subjectList.appendChild(listItem);
-      });
-    })
-    .catch(error => console.error("Error fetching subjects:", error));
-
-  // Function to display the image in the content section
-  function displayImage(imageURL) {
-    const contentSection = document.querySelector(".content");
+function displayImage(imageURL) {
+    const contentSection = document.querySelector(".map-image");
     contentSection.innerHTML = `<img src="${imageURL}" alt="Map Image">`;
-  }
-});
-
+}
