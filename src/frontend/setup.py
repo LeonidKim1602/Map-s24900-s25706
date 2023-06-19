4c6dd2f67096523e2ec44e16fa64ba8af0068052
Leonid Kim <s24900@pjwstk.edu.pl>
2023-06-19 14:42:02 +0200

    Add requirements and setup file to the frontend.

---
+++ src/frontend/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pjait_map_frontend",
+    version="0.0.1",
+    author="Leonid Kim",
+    author_email="s24900@pjwstk.edu.pl",
+    description="Interactive map of PJAIT frontend part",
+    url="https://github.com/LeonidKim1602/Map-s24900-s25706",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+    ],
+    python_requires=">=3.10",
+)
