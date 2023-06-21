cf5485941edd8335782d8314efb7a30b211b11db
Le0-dot <lkolyadich@gmail.com>
2023-06-21 18:44:27 +0200

    Add check for user existance in /user/new endpoint.

--- src/backend/pjait_map_backend/routers/students.py
+++ src/backend/pjait_map_backend/routers/students.py
@@ -28,5 +28,9 @@ async def get_student(
 async def create_student(
     user: CreateUser, db: Session = Depends(DatabaseManager.get_db)
 ) -> Response:
+    student = crud.get_student(user.number, db)
+    if student is not None:
+        return Response(status_code=403)
+
     crud.add_student(user, db)
     return Response(status_code=201)
