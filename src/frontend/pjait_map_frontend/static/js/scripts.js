ad800757f10f13f56930801ff5efa1fdf3d14cb4
Leonid Kim <s24900@pjwstk.edu.pl>
2023-06-25 17:53:07 +0200

    Fixed button margins on the main page. Selection boxes and time inputs on the timetable page are now aligned. Unnecessary scripts are deleted.

--- src/frontend/pjait_map_frontend/static/js/scripts.js
+++ src/frontend/pjait_map_frontend/static/js/scripts.js
@@ -9,29 +9,22 @@ function closePopup() {
   popup.style.display = "none";
 }
 
-function addActivity(dayId) {
-  openPopup("add");
-  document.getElementById("weekday").value = dayId;
-}
+
 
 
 
 function deleteActivity(scheduleId) {
-  // Send a DELETE request to the server to delete the schedule
   fetch(`/timetable/${scheduleId}`, {
     method: 'DELETE',
   })
   .then(response => {
     if (response.ok) {
-      // Refresh the page after successful deletion
       location.reload();
     } else {
-      // Handle error response
       console.error('Failed to delete schedule:', response.statusText);
     }
   })
   .catch(error => {
-    // Handle network or other errors
     console.error('An error occurred while deleting the schedule:', error);
   });
 }
