eba008bcc83ca0dabb641bc073038b625bf08a43
Leonid Kim <s24900@pjwstk.edu.pl>
2023-06-26 09:57:26 +0200

    Added small descriptions of maps and a button to show all maps on the main page.

--- src/frontend/pjait_map_frontend/static/js/main.js
+++ src/frontend/pjait_map_frontend/static/js/main.js
@@ -10,4 +10,44 @@ function displayImage(imageURL, locX, locY) {
   const offsetY = rect.top;
   const pin = `<div class="pin" style="left: ${offsetX + locX - 20}px; top: ${offsetY + locY - 20}px;"></div>`;
   contentSection.innerHTML += pin;
+
+  const descriptionSection = document.getElementById("description");
+  let text = "Description not found."
+  switch(imageURL){
+    case "http://127.0.0.1:8001/img/A0.png":
+      text = "Floor 0 in builduing A (Koszykowa 86).";
+      break;
+    case "http://127.0.0.1:8001/img/A1.png":
+      text = "Floor 1 in builduing A (Koszykowa 86).";
+      break;
+    case "http://127.0.0.1:8001/img/A2.png":
+      text = "Floor 2 in builduing A (Koszykowa 86).";
+      break;
+    case "http://127.0.0.1:8001/img/A3.png":
+      text = "Floor 3 in builduing A (Koszykowa 86).";
+      break;
+    case "http://127.0.0.1:8001/img/A3.png":
+      text = "Floor 4 in builduing A (Koszykowa 86).";
+      break;
+    default:
+      break;
+  }
+
+descriptionSection.innerHTML = text;
+}
+
+function displayAll(){
+  const contentSection = document.querySelector(".map-image");
+
+  let image = `<img src="http://127.0.0.1:8001/img/A0.png" alt="Map Image" id="img">`;
+  image += `<img src="http://127.0.0.1:8001/img/A1.png" alt="Map Image" id="img">`;
+  image += `<img src="http://127.0.0.1:8001/img/A2.png" alt="Map Image" id="img">`;
+  image += `<img src="http://127.0.0.1:8001/img/A3.png" alt="Map Image" id="img">`;
+  image += `<img src="http://127.0.0.1:8001/img/A4.png" alt="Map Image" id="img">`;
+  contentSection.innerHTML = image;
+
+  const descriptionSection = document.getElementById("description");
+  let text = "These are all the maps."
+  descriptionSection.innerHTML = text;
+
 }
